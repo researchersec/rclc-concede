adcfa795769ed6460c2e28076e2615950a90c95c
researchersec <109282836+researchersec@users.noreply.github.com>
2023-09-24 01:50:01 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+let data = [];
+
+// Load JSON data from the repository
+fetch('export.json')
+    .then(response => response.json())
+    .then(jsonData => {
+        data = jsonData;
+    })
+    .catch(error => {
+        console.error("Error fetching the JSON data: ", error);
+    });
+
+function searchPlayer() {
+    const playerName = document.getElementById("playerName").value;
+    const resultsTable = document.getElementById("resultsTable");
+    
+    // Filter data for the player
+    const playerData = data.filter(entry => entry.player === playerName);
+
+    // Create table rows from the player data
+    let tableContent = '<tr><th>Date</th><th>Item Name</th><th>Votes</th><th>Instance</th><th>Boss</th></tr>';
+    playerData.forEach(entry => {
+        tableContent += `<tr><td>${entry.date}</td><td>${entry.itemName}</td><td>${entry.votes}</td><td>${entry.instance}</td><td>${entry.boss}</td></tr>`;
+    });
+    
+    resultsTable.innerHTML = tableContent;
+}
