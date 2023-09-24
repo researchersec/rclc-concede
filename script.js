acb38970fdcac88f2755209de4c305fd7357210b
researchersec <109282836+researchersec@users.noreply.github.com>
2023-09-24 03:14:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,27 +18,25 @@ function searchPlayer() {
 
     let playerName = document.getElementById("playerName").value;
     playerName += "-Golemagg";  // Appending -Golemagg to the entered player name
-    
-    const resultsTable = document.getElementById("resultsTable");
-    
-    // Filter data for the player
-    const playerData = data.filter(entry => entry.player === playerName);
 
-    // Ensure tableContent is initialized before the loop
-    let tableContent = '<tr><th>Date</th><th>Item Name</th><th>Votes</th><th>Instance</th><th>Boss</th></tr>';
+    // Resetting the DataTable and table content
+    if ($.fn.dataTable.isDataTable('#resultsTable')) {
+        $('#resultsTable').DataTable().destroy();
+    }
+    $('#resultsTable tbody').empty();
+
+    let tableContent = '';
 
     playerData.forEach(entry => {
-        let itemId = entry.itemId ? entry.itemId.toString() : "117378";  // Ensure itemId is a string or use default value
+        let itemId = entry.itemId ? entry.itemId.toString() : "117378";
         let wowheadLink = `<a href="https://www.wowhead.com/item=${itemId}" data-wowhead="item=${itemId}" target="_blank">${entry.itemName}</a>`;
         tableContent += `<tr><td>${entry.date}</td><td>${wowheadLink}</td><td>${entry.votes}</td><td>${entry.instance}</td><td>${entry.boss}</td></tr>`;
     });
-    
-    resultsTable.innerHTML = tableContent;
+
+    $('#resultsTable tbody').html(tableContent);
 
     // Initialize the DataTable and set default sorting
     $('#resultsTable').DataTable({
-        "order": [[0, 'desc']],  // Sort by first column (Date) in descending order
-        "destroy": true  // Destroy any existing table instance before reinitializing
+        "order": [[0, 'desc']]  // Sort by first column (Date) in descending order
     });
-
 }
