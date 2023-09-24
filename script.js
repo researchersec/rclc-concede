1b644a3bf812dabf8c90927b3ade24c9f958ef83
researchersec <109282836+researchersec@users.noreply.github.com>
2023-09-24 02:24:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ fetch('export.json')
     });
 
 function searchPlayer() {
+    if (!data.length) {
+        console.log("Data not loaded yet. Please try again in a moment.");
+        return;
+    }
+
     let playerName = document.getElementById("playerName").value;
     playerName += "-Golemagg";  // Appending -Golemagg to the entered player name
     
@@ -22,16 +27,14 @@ function searchPlayer() {
     // Create table rows from the player data
     let tableContent = '<tr><th>Date</th><th>Item Name</th><th>Votes</th><th>Instance</th><th>Boss</th></tr>';
     playerData.forEach(entry => {
-    if (entry.itemId) {
-        let wowheadLink = `<a href="https://www.wowhead.com/item=${entry.itemId}" target="_blank">${entry.itemName}</a>`;
-        tableContent += `<tr><td>${entry.date}</td><td>${wowheadLink}</td><td>${entry.votes}</td><td>${entry.instance}</td><td>${entry.boss}</td></tr>`;
-    } else {
-        // If itemId is missing, just display the item name without a link
-        tableContent += `<tr><td>${entry.date}</td><td>${entry.itemName}</td><td>${entry.votes}</td><td>${entry.instance}</td><td>${entry.boss}</td></tr>`;
-    }
-});
-
-
+        if (entry.itemId) {
+            let wowheadLink = `<a href="https://www.wowhead.com/item=${entry.itemId}" target="_blank">${entry.itemName}</a>`;
+            tableContent += `<tr><td>${entry.date}</td><td>${wowheadLink}</td><td>${entry.votes}</td><td>${entry.instance}</td><td>${entry.boss}</td></tr>`;
+        } else {
+            console.log("Missing itemId for item:", entry.itemName);
+            tableContent += `<tr><td>${entry.date}</td><td>${entry.itemName}</td><td>${entry.votes}</td><td>${entry.instance}</td><td>${entry.boss}</td></tr>`;
+        }
+    });
     
     resultsTable.innerHTML = tableContent;
 
