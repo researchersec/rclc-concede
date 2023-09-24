4dc3517c53ee5c427f0d32738d5c4b680d4b9ec5
researchersec <109282836+researchersec@users.noreply.github.com>
2023-09-24 03:34:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -32,10 +32,13 @@ function searchPlayer() {
     let tableContent = '';
 
     playerData.forEach(entry => {
-        let itemId = entry.itemId ? entry.itemId.toString() : "117378";
+        console.log("Processing entry:", entry);  // Log the entire entry
+        console.log("Item ID:", entry.itemId);  // Log the itemId of the entry
+    
+        let itemId = entry.itemId ? entry.itemId.toString() : "117378";  // Ensure itemId is a string or use default value
         let wowheadLink = `<a href="https://www.wowhead.com/item=${itemId}" data-wowhead="item=${itemId}" target="_blank">${entry.itemName}</a>`;
         tableContent += `<tr><td>${entry.date}</td><td>${wowheadLink}</td><td>${entry.votes}</td><td>${entry.instance}</td><td>${entry.boss}</td></tr>`;
-    });
+});
 
     $('#resultsTable tbody').html(tableContent);
 
